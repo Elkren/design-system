72777ce12c8170954600f240d1c9718ac9232842
Elkren <sam.pallesclark@gmail.com>
2020-09-11 14:17:01 +0100

    Styled components added + Primary button

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import PrimaryButton from "./components/Buttons";
 
-const App = () => <h1>Hello world</h1>;
+const App = () => <PrimaryButton>Hello world</PrimaryButton>;
 
 ReactDOM.render(<App />, document.querySelector("#root"));
